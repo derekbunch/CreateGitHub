4a455c8d924c795e622297c18bec128d41867a47
derek.Ubuntu <derekbunch93@gmail.com>
2019-07-20 00:35:43 -0500

    Add remove and update to github API

--- CreateGit.py
+++ CreateGit.py
@@ -1,17 +1,7 @@
 import sys, os
 import json
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from github import Github
 import time
-
-# def setup(installLocation):
-#     with open(installLocation + '/bin/config.json', 'r+') as config:
-#         data = json.load(config)
-#         install = data['folders']
-#         install['install'] = installLocation
-#         config.seek(0, 0)
-#         json.dump(config, data)
-#         config.truncate()
 
 def newLanguage (language):
     print('This is a new language!')
@@ -30,9 +20,9 @@ def create():
 
     with open(bin + 'config.json') as config:
         data = json.load(config)
-        #setup(os.path.dirname(sys.argv[0]))
         username = data['credentials']['username']
         password = data['credentials']['password']
+        token = data['credentials']['accesstoken']
         projectsFolder = data['folders']['projects']
 
     # Set variables for passed argument for reuse
@@ -64,36 +54,46 @@ def create():
         elif (new == 'n'):
             projectName = input('What would you like the new name to be? ')
     
-    options = Options()
-    options.headless = True
-    browser = webdriver.Firefox()#options=options)#,firefox_binary=bin)
-    browser.get('http://github.com/login')
+        if (token is None):
+            g = Github(username, password)
+        else:
+            g = Github(token)
+        user = g.get_user()
+        user.create_repo('\n' + projectName)
 
-    pyButton = browser.find_element_by_xpath('//*[@id="login_field"]')
-    pyButton.send_keys(username)
-    pyButton = browser.find_element_by_xpath('//*[@id="password"]')
-    pyButton.send_keys(password)
-    pyButton = browser.find_element_by_xpath('/html/body/div[3]/main/div/form/div[3]/input[4]')
-    pyButton.click()
-    pyButton = browser.find_element_by_xpath('/html/body/div[4]/div/aside[1]/div[2]/div/div/h2/a')
-    pyButton.click()
-    pyButton = browser.find_element_by_xpath('//*[@id="repository_name"]')
-    pyButton.send_keys(projectName)
-    pyButton = browser.find_element_by_xpath('//*[@id="repository_visibility_private"]')
-    pyButton.click()
-    time.sleep(1)
-    pyButton = browser.find_element_by_xpath('/html/body/div[4]/main/div/form/div[3]/button')
-    pyButton.click()
-    browser.close()
+        # git actions
+        os.chdir(projectName)
+        os.system('git init')
+        os.system('git remote add origin git@github.com:derekbunch/{}.git'.format(projectName))
+        os.system('touch README.md')
+        os.system('git add .')
+        os.system('git commit -m "Initial Commit"')
+        os.system('git push -u origin master')
+        #os.system('code-insiders .')
+        print('{} repo created!'.format(projectName))
 
-    os.chdir(projectName)
-    os.system('git init')
-    os.system('git remote add origin git@github.com:derekbunch/{}.git'.format(projectName))
-    os.system('touch README.md')
-    os.system('git add .')
-    os.system('git commit -m "Initial Commit"')
-    os.system('git push -u origin master')
-    #os.system('code .')
+def delete():
+    bin = os.path.dirname(sys.argv[0]) + '/bin/'
+
+    with open(bin + 'config.json') as config:
+        data = json.load(config)
+        username = data['credentials']['username']
+        password = data['credentials']['password']
+        token = data['credentials']['accesstoken']
+        projectsFolder = data['folders']['projects']
+    projectName = sys.argv[1]
+
+    if (token is None):
+        g = Github(username, password)
+    else:
+        g = Github(token)
+    repo = g.get_repo(username + '/' + projectName)
+    repo.delete()
+
 
 if __name__ == '__main__':
-    create()
+    data = []
+    if(sys.argv[3] == '-c'):
+        create()
+    elif (sys.argv[3] == '-d'):
+        delete()
