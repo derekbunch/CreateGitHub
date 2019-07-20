0880b86c0f708915161e879a1c2f3e6c5ff6fc7d
derek.Ubuntu <derekbunch93@gmail.com>
2019-07-20 17:09:31 -0500

    implement setup and config file creation

--- CreateGit.py
+++ CreateGit.py
@@ -3,6 +3,38 @@ import json
 from github import Github
 import time
 
+def setup():
+    print('---------------')
+    print(' Initial Setup ')
+    print('---------------')
+    installFolder = sys.argv[0].replace('CreateGit.py','')
+    projectsFolder = input('Where would you like projects to be saved?\n')
+    authMethod = input('Would you like to use an access token (instead of Username and Password)? (y/n)\n')
+    username, password, token = '', '', ''
+    while (authMethod != 'y' and authMethod != 'n'):
+        authMethod = input("Please choose (y) or (n): ")
+    if (authMethod == 'y'):
+        token = input('Access Token: ')
+    elif (authMethod == 'n'):
+        username = input('Username: ')
+        password = input('Password: ')
+    
+    data = {}
+    data['credentials'] = {
+        'username': username,
+        'password' : password,
+        'accesstoken' : token
+    }
+    data['languages'] = {}
+    data['folders'] = {
+        'install' : installFolder,
+        'projects' : projectsFolder
+    }
+
+    with open(bin + 'config.json', 'w') as config:
+        json.dump(data, config, sort_keys=True, indent=4)
+
+
 def newLanguage (language):
     print('This is a new language!')
     new = input('Would you like to create a new directory for this language? (y/n): ')
@@ -16,8 +48,6 @@ def newLanguage (language):
         sys.exit(1)
 
 def create():
-    bin = os.path.dirname(sys.argv[0]) + '/bin/'
-
     with open(bin + 'config.json') as config:
         data = json.load(config)
         username = data['credentials']['username']
@@ -40,7 +70,6 @@ def create():
         newLanguage(language)
 
     os.chdir('./{}'.format(language))
-    print(os.getcwd())
     try:
         os.mkdir('./{}'.format(projectName))
     except:
@@ -60,7 +89,7 @@ def create():
     else:
         g = Github(token)
     user = g.get_user()
-    user.create_repo('\n' + projectName)
+    user.create_repo(projectName)
 
     # git actions
     os.chdir(projectName)
@@ -71,7 +100,7 @@ def create():
     os.system('git commit -m "Initial Commit"')
     os.system('git push -u origin master')
     #os.system('code-insiders .')
-    print('{} repo created!'.format(projectName))
+    print('\n{} repo created!\n'.format(projectName))
 
 def delete():
     bin = os.path.dirname(sys.argv[0]) + '/bin/'
@@ -89,10 +118,16 @@ def delete():
         g = Github(token)
     repo = g.get_repo(username + '/' + projectName)
     repo.delete()
+    print('\n{} repo deleted!\n'.format(projectName))
 
 
 if __name__ == '__main__':
     data = []
+    bin = os.path.dirname(sys.argv[0]) + '/bin/'
+
+    if (not os.path.isfile(bin + 'config.json')):
+        setup()
+
     if(sys.argv[3] == '-c'):
         create()
     elif (sys.argv[3] == '-d'):
